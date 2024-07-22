8c43f9d0ce00876b66f6f974b292f10ad1762f79
Anthony Moriarty <panzerfowst@gmail.com>
2024-07-22 18:00:22 -0500

    chore: removed unneeded options from listFiles.ps1 script

--- app/model/listFiles.ps1
+++ app/model/listFiles.ps1
@@ -7,36 +7,12 @@ param(
         return $true
     })]
     [System.IO.DirectoryInfo]$input_path,
-    [System.IO.DirectoryInfo]$output_path,
-    [string]$recursive = "false", # TODO: Add this option later...
-    [string]$is_safe_mode = "true", # default value if not assigned
-    [string]$is_creating_new_directories = "false",
-    [string]$is_deleting_empty_directories = "false",
-    [string]$is_moving_files = "false",
-    [string]$is_adding_count_str = "false",
-    [string]$is_adding_date_str = "false",
+    [string]$recursive = "false", # default value if not assigned
     $envname
 )
 
 # Convert string boolean values to actual Booleans
 [bool]$recursive = [System.Convert]::ToBoolean($recursive)
-[bool]$is_safe_mode = [System.Convert]::ToBoolean($is_safe_mode)
-[bool]$is_creating_new_directories = [System.Convert]::ToBoolean($is_creating_new_directories)
-[bool]$is_deleting_empty_directories = [System.Convert]::ToBoolean($is_deleting_empty_directories)
-[bool]$is_moving_files = [System.Convert]::ToBoolean($is_moving_files)
-[bool]$is_adding_count_str = [System.Convert]::ToBoolean($is_adding_count_str)
-[bool]$is_adding_date_str = [System.Convert]::ToBoolean($is_adding_date_str)
-
-Write-Host "`n"
-Write-Host "Looking in '$input_path'..."
-Write-Host "Output Path: '$output_path'..."
-Write-Host "Recursive Search = $recursive"
-Write-Host "Safe Mode = $is_safe_mode"
-Write-Host "New Directories = $is_creating_new_directories"
-Write-Host "Deleting Empty = $is_deleting_empty_directories"
-Write-Host "Moving Files = $is_moving_files"
-Write-Host "Adding Count = $is_adding_count_str"
-Write-Host "Adding Date = $is_adding_date_str"
 
 
 # -Recurse looks into all subdirectories.  Leave the option out to only look at current
